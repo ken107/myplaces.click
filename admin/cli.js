9417d73fb4038757d5d93976a1704147e6083d1d
Hai Phan <hai.phan@gmail.com>
2022-05-15 18:12:56 +0700

    first

--- admin/cli.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-require('dotenv').config();
-
-require('covid-admin').startServer({
-    knexConfig: require('./knexfile.js'),
-});
--- admin/src/args.ts
+++
@@ -1,9 +0,0 @@
-import minimist from 'minimist';
-
-const args: {
-    knexFile: string,
-    env: string,
-    _: string[],
-} = minimist(process.argv.slice(2)) as any;
-
-export default args;
